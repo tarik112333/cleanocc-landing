9a174094dbc152e92cf3efd9c58f7b0e81abe4d6
tarik112333 <97233518+tarik112333@users.noreply.github.com>
2026-04-06 21:58:21 +0200

    Inline sitemap.xml content in server route

--- server.js
+++ server.js
@@ -137,7 +137,15 @@ app.get('/app.js', (req, res) => {
 });
 
 app.get('/sitemap.xml', (req, res) => {
-  res.type('application/xml').sendFile(path.join(__dirname, 'sitemap.xml'));
+  res.type('application/xml').send(`<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://cleanocc.fr/</loc>
+    <lastmod>2026-04-06</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`);
 });
 
 // --- ROUTE HTML ---
